b154c6e4ff7f7af1957c8c45dfe0a30aa14bb86f
J.D. Britton <75180464+jdbritton@users.noreply.github.com>
2022-01-29 12:34:45 +0800

    Update quickStart.py

--- quickStart.py
+++ quickStart.py
@@ -13,7 +13,9 @@ import webbrowser
 import subprocess
 import time
 import os
-
+import colorama
+from colorama import Fore
+colorama.init()
 
 # ==============================================================================
 #   Variables go here:
@@ -21,7 +23,7 @@ import os
 
 now = d8.now()
 
-# Below: absolute path to Firefox (or other desired browser)
+# Absolute path to Firefox (or other desired browser)
 firefox_path = "C:\\Program Files\\Firefox Developer Edition\\Firefox.exe"
 
 # Registering the above path as a browser for webbrowser module.
@@ -35,16 +37,16 @@ exitval = 0
 # See the runas_alt() function
 
 # Define the programs to open -- if program is not in PATH, use absolute path:
-programs = (["C:\\Program Files (x86)\\KeePass Password Safe 2\\KeePass.exe", 
+programs = ("C:\\Program Files \(x86\)\\KeePass Password Safe 2\\KeePass.exe", 
     "C:\\Program Files\\AutoHotkey\\AutoHotkey.exe",
-    "C:\\Program Files\\Remote Desktop\\msrdcw.exe",
-    "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\Outlook.exe",
-    'C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\EXCEL.exe "C:\\Users\\jambri\\Desktop\\userlist.lnk"'])
+    #"C:\\Program Files\\Remote Desktop\\msrdcw.exe",
+    "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\Outlook.exe")
+    #"C:\\Program Files (x86\\\Microsoft\\Remote Desktop Connection Manager\\RDCMan.exe")
 
 # Define the pages to open, one per line:
-pages = (["google.com", 
+pages = ("google.com", 
     "communities.service-now.com/ticket.do", 
-    "reddit.com"])
+    "reddit.com")
 
 # Define the alternate accounts that will be used with "runas"
 alt_account0 = "head_office\\jabritton.adm" # First alternate account
@@ -52,12 +54,10 @@ alt_account1 = "head_office\\jabritton.adm" # Second alternate account
 
 # Commands to be used with "runas" - alt_account0 first.
 com_lines0 = (['"powershell.exe start-process mmc dsa.msc -verb runas"',
-    '"powershell.exe start-process powershell -verb runas"',
-    '"powershell.exe start-process C:\Temp\CmRcViewer.exe.lnk -verb runas"',
     '"powershell.exe start-process C:\Temp\WindowsTerminal.exe.lnk -verb runas"'])
 
 # Commands to be used with "runas" - alt_account1 next.
-com_lines1 = (["notepad.exe"])
+com_lines1 = (['"powershell.exe start-process C:\Temp\sccmc.lnk -verb runas"'])
 
 
 # ==============================================================================
@@ -68,41 +68,42 @@ com_lines1 = (["notepad.exe"])
 def openbrowser(url):
     try:
         webbrowser.get('firefox').open(url)
+        print(Fore.YELLOW + "Opening webpage: " + str(url) + Fore.RESET)
     except:
-        print("Failed to open web browser to: " + str(url) + "!") # We have output on-screen so we know if something breaks
+        print(Fore.RED + "Failed to open web browser to: " + str(url) + "!") # We have output on-screen so we know if something breaks
         exitval = 1
         print(str(exitval) + " -- error occurred. Continuing regardless.") 
         print("=========================================================================")
-        print("   ")
+        print(Fore.RESET + "   ")
     finally:
-        print("Opening webpage: " + str(url))
         print("=========================================================================")
-        print("   ")
+        print(Fore.RESET + "   ")
 
 
 # Function to open programs.
 def openprograms(prog):
     try:
         subprocess.Popen(prog)
+        print(Fore.CYAN + "Opening program: " + str(prog) + Fore.RESET)
     except:
-        print("Failed to open " + str(prog) + "!")
+        print(Fore.RED + "Failed to open " + str(prog) + "!")
         exitval = 1
         print(str(exitval) + " -- error occurred. Continuing regardless.")
         print("=========================================================================")
-        print("   ")
+        print("   " + Fore.RED)
     finally:
-        print("Opening program: " + str(prog))
         print("=========================================================================")
-        print("   ")
+        print("   " + Fore.RED )
 
 
 # Function to open things with Runas.
 def runas_alt(com_line, alt_account):
     try:
+        print(Fore.MAGENTA)
         os.system('runas /user:{} /savecred {}'.format(alt_account, com_line)) 
-        print("   ")
+        print("" + Fore.RESET, end="")
     except:
-        print("Failed to execute runas with the following parameters: {}".format(com_line))
+        print(Fore.RED + "Failed to execute runas with the following parameters: {}".format(com_line) + Fore.RESET)
     finally:
         print("=========================================================================")
         print("   ")
@@ -112,8 +113,9 @@ def runas_alt(com_line, alt_account):
 #   Main section: 
 # ==============================================================================
 
-print("Welcome back to the grind, Lord Foxington~")
-print("It's currently: " + str(now))
+print(Fore.GREEN + "Welcome back to the grind, Lord Foxington~")
+print(Fore.GREEN + "It's currently: " + str(now) + Fore.RESET)
+print("   ")
 
 
 # ==============================================================================
@@ -135,15 +137,15 @@ print("===============        Opening necessary programs:        ===============
 
 for program in programs:
     openprograms(program)
-print("   ")
+print("   " + Fore.RESET)
 
 
 # ==============================================================================
 #   Opening batch files or whatever is needed to open things as other users:
 # ==============================================================================
 
-print("===============        Opening Apps as Other Users        ===============")
-print("   ")
+print(Fore.RESET + "===============        Opening Apps as Other Users        ===============")
+print("   " + Fore.RESET)
 
 for com_line in com_lines0:
     runas_alt(com_line,alt_account0)
@@ -157,7 +159,7 @@ for com_line in com_lines1:
 
 time.sleep(3)
 
-print("Finished. Go kick some tail.")
+print(Fore.RED + "Finished. Go kick some tail.")
 print("   ")
 print("=========================================================================")
 print("   ")
@@ -168,6 +170,6 @@ print("  `-.}   `=._,.-=-._.,  @ @._,")
 print("     `._ _,-.   )       _,.'")
 print("        `    G.m` m`m'")
 
-time.sleep(30)
+input("Press enter key to exit." + Fore.RESET)
 exit(exitval)
 # ==============================================================================
